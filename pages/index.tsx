d6fd9c19794c7f001bb86a0efcb093b32256de0f
annisann <hello.annisann@gmail.com>
2023-04-07 02:10:46 +0700

    💭 fetch api and display it on page, add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,32 @@
 import Head from 'next/head'
-import { Button } from '@nextui-org/react'
+import { Text, Button, Spacer } from '@nextui-org/react'
 import { useState } from 'react'
 
 export default function Home() {
 	const [caption, setCaption] = useState("")
 
-	const getCaption = async() => {
+	const getCaption = async () => {
 		await fetch("https://binaryjazz.us/wp-json/genrenator/v1/story/")
+			.then(async (response) => await response.json())
+			.then(result => setCaption(result))
 	}
 
 	return (
 		<>
 			<Head>
-				<title> caption generator the odd </title>
-				<meta name="description" content="Made with NASA TechPort API" />
+				<title> caption genrerator </title>
+				<meta name="description" content="made with genrerator api" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
-				<p> {caption} </p>
-				<Button> generate </Button>
+				<Text b size={18}> {caption} </Text>
+				<Spacer/>
+				<Button onPress={getCaption}> generate </Button>
 			</main>
+			<footer>
+				<p> made with genrenator api </p>
+			</footer>
 		</>
 	)
 }
